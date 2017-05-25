4d0c24700882856309be0e9086c46ddd5f8d5a8c
Edwin Mendez <mendezwin@gmail.com>
2017-05-25 12:49:48 -0600

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    CacheBuster = require('gulp-cachebust'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    print = require('gulp-print'),
+    babel = require('gulp-babel'),
+    cachebust = new CacheBuster()
+
+gulp.task('hello', function(){
+    console.log('Hello world');
+})
+
+gulp.task('build-css', function(){
+    return gulp.src('./styles/*')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(cachebust.resources())
+        .pipe(concat('styles.css'))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('build-js', function() {
+   return gulp.src('js/**/*.js')               
+      .pipe(sourcemaps.init())
+      .pipe(print())                        
+      .pipe(babel({ presets: ['es2015'] }))
+      .pipe(concat('bundle.js'))
+      //.pipe(uglify())
+      .pipe(sourcemaps.write('./')) 
+      .pipe(gulp.dest('./dist/js')); 
+});
+
+gulp.task('build', ['build-css', 'build-js'], function() {
+    return gulp.src('index.html')
+        .pipe(cachebust.references())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    return gulp.watch(['./index.html','./partials/*.html', './styles/*.*css', './js/**/*.js'], ['build']);
+});
